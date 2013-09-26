d0f1d395d88b66af7033aa93afab46a8add7456d
Michael Chittenden <mchitten@gmail.com>
2013-09-26 10:36:50 -0400

    Removes the DsAuth folder

--- DsAuth/app/assets/javascripts/ds_auth/html5.js
+++
@@ -1,2 +0,0 @@
-// HTML5 Shiv for HTML5 Tag integration in IE 8
-//= require ./html5shiv
--- DsAuth/config/routes.rb
+++
@@ -1,7 +0,0 @@
-DsAuth::Engine.routes.draw do
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  get 'logout', to: 'sessions#destroy'
-
-  get 'gate-test/blank', to: 'static_pages#blank'
-end
